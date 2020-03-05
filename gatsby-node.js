29c6705392403bb04795108bd78edc03468ed358
jaedung <justinkim0310@gmail.com>
2020-03-05 21:37:45 +1000

    building our blog 3 in progress

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPage = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    query MyQuery {
+      allMarkdownRemark {
+        totalCount
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+}
